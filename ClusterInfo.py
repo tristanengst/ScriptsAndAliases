8457fb2738c79add5b34c8e8f07f7839f9391197
Tristan Engst <tristanengst@gmail.com>
2025-10-18 06:46:09 -0400

    Nicer prints

--- ClusterInfo.py
+++ ClusterInfo.py
@@ -58,10 +58,10 @@ def resource_infos_to_str(time2resource2info, show_nodes=0, max_nodes_to_show=5,
 
 
     to_print = ""
-    max_str_len = max([len(s) for s in strs]) if len(strs) > 0 else 0
+    max_str_len = max([len(s) + len("\t\t") for s in strs]) if len(strs) > 0 else 0
     for idx,(time,resource_strs) in enumerate(time2printed_resources.items()):
         to_print += "\n\t\t\t\t" if idx > 0 else ""
-        to_print += "".join([r.ljust(max_str_len) + "\t\t" for r in resource_strs])
+        to_print += "".join([r.ljust(max_str_len) for r in resource_strs])
     
     s = f"Free/Avail/Total:\t{to_print}"
     return s
