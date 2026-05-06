969ad9b5dabe6e0d0d17d51acccab9beb0ab7c0a
Tristan Engst <tristanengst@gmail.com>
2026-05-05 18:24:00 -0700

    Update

--- UserConfig.py
+++ UserConfig.py
@@ -38,9 +38,10 @@ checkpoints_search_dirs = ["~/scratch/IMLE-SSL/models_imle",
     "~/scratch/IMLE-SSL/segmentation",
     "~/Development/IMLE-SSL-2/probes",
     "~/scratch/openPiMLE/exps",
-    "~/scratch/LeRobot/exps"]
+    "~/scratch/LeRobot/exps",
+    "~/Development/NewMetric/generated_images"]
 job_result_search_dirs = [
-    "~/Development/IMLE-SSL-2/pretrain_results",
+    "~/Development/IMLE-SSL-2/pretrain_results", "~/Development/IMLE-SSL-2/job_results",
     "~/Development/IMLE-SSL-2/finetune_results",
     "~/Development/IMLE-SSL-2/probe_results",
     "~/Development/IMLE-SSL-2/segmentation_results",
@@ -49,7 +50,8 @@ job_result_search_dirs = [
     "~/Development/IMLE-SSL-Dev/probe_results",
     "~/Development/IMLE-SSL-Dev/segmentation_results",
     "~/Development/openPiMLE-lerobot/results",
-    "~/Development/LeRobot/results"
+    "~/Development/LeRobot/results",
+    "~/Development/NewMetric/job_results",
 ]
 job_error_search_dirs = [
     "~/Development/openPiMLE-lerobot/errors"
@@ -58,7 +60,8 @@ slurm_script_search_dirs = [
     "~/Development/IMLE-SSL-2/slurm",
     "~/Development/IMLE-SSL-Dev/slurm",
     "~/Development/openPiMLE-lerobot/slurm",
-    "~/Development/LeRobot/slurm"
+    "~/Development/LeRobot/slurm",
+    "~/Development/NewMetric/slurm",
 ]
 
 checkpoints_search_dirs = [osp.expanduser(p) for p in checkpoints_search_dirs]
