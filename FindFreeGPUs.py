18997fab03eff610f06461701b0257e098d3e1ca
Tristan Engst <tristanengst@mgmail.com>
2026-05-23 03:53:32 +0000

    Handles errors better

--- FindFreeGPUs.py
+++ FindFreeGPUs.py
@@ -20,21 +20,35 @@ try:
 except ImportError:
     def tqdm(x): return x
 
+class NvidiaSMIError(Exception):
+    """Error related to nvidia-smi command execution."""
+    pass
+
 def gpu_uid_to_index(h=None):
     """Returns a dictionary mapping GPU UIDs to their indices."""
     cmd = "nvidia-smi --query-gpu=index,gpu_uuid --format=csv,noheader"
-    result = SSHCommunication.run_command_on_machine(machine=h, command=cmd, if_connect_error=None, if_ssh_map_error=None)
+    result = SSHCommunication.run_command_on_machine(machine=h, command=cmd,
+        if_connect_error="HostInfoError",
+        if_ssh_map_error="HostInfoError")
+    result = result.strip() if result else None
     if result is None:
         return dict()
+    elif not result[0].isdigit():
+        raise NvidiaSMIError(f"nvidia-smi issue for host={h}: {result}")
     else:
         lines = [l.split(",") for l in result.strip().splitlines()]
         return {gpu_uid.strip(): int(gpu_idx.strip()) for gpu_idx, gpu_uid in lines}
 
 def gpu_uid_to_procids(h=None):
     cmd = "nvidia-smi --query-compute-apps=gpu_uuid,pid --format=csv,noheader"
-    result = SSHCommunication.run_command_on_machine(machine=h, command=cmd, if_connect_error=None, if_ssh_map_error=None)
+    result = SSHCommunication.run_command_on_machine(machine=h, command=cmd,
+        if_connect_error="HostInfoError",
+        if_ssh_map_error="HostInfoError")
+    result = result.strip() if result else None
     if result is None:
         return dict()
+    elif not result.startswith("GPU-"):
+        raise NvidiaSMIError(f"nvidia-smi issue for host={h}: {result}")
     else:
         lines = [l.split(",") for l in result.strip().splitlines()]
         result = defaultdict(list)
@@ -46,22 +60,37 @@ def procids_to_users(*procids, h=None):
     if len(procids) == 0:
         return dict()
     cmd = f"ps -o pid,user --no-headers -p {','.join([str(pid) for pid in procids])}"
-    result = SSHCommunication.run_command_on_machine(machine=h, command=cmd, if_connect_error=None, if_ssh_map_error=None)
+    result = SSHCommunication.run_command_on_machine(machine=h, command=cmd,
+        if_connect_error="HostInfoError",
+        if_ssh_map_error="HostInfoError")
     if result is None:
         return dict()
     else:
         return {l.split()[0]: l.split()[1] for l in result.strip().splitlines()}
 
 def gpu_index_to_users(h=None):
-    gpu_uid2index = gpu_uid_to_index(h=h)
-    gpu_uid2procids = gpu_uid_to_procids(h=h)
-    gpu_index2procids = {gpu_uid2index[gpu_uid]: gpu_uid2procids.get(gpu_uid, []) for gpu_uid in gpu_uid2index.keys()}
-    gpu_index2users = {gpu_idx: sorted(procids_to_users(*procids, h=h).values()) for gpu_idx, procids in gpu_index2procids.items()}
-    return gpu_index2users
+    try:
+        gpu_uid2index = gpu_uid_to_index(h=h)
+        gpu_uid2procids = gpu_uid_to_procids(h=h)
+        gpu_index2procids = {gpu_uid2index[gpu_uid]: gpu_uid2procids.get(gpu_uid, []) for gpu_uid in gpu_uid2index.keys()}
+        gpu_index2users = {gpu_idx: sorted(procids_to_users(*procids, h=h).values()) for gpu_idx, procids in gpu_index2procids.items()}
+        return gpu_index2users
+    except SSHCommunication.HostInfoError as e:
+        return str(e)
+    except NvidiaSMIError as e:
+        return str(e)
+    except Exception as e:
+        twrite(f"Unexpected error for host={h}: {e}")
+        return dict()
 
 def machine_gpu_usage_summary_str(*, machine2gpu_index2users=None):
     if machine2gpu_index2users is None:
         machine2gpu_index2users = {SSHCommunication.get_machine_name(): gpu_index_to_users()}
+
+    # Separate out the machines that had some issue, eg. SSH connection or nvidia-smi,
+    # and handle them later.
+    machine2error_strs = {m: gpu_index2users for m, gpu_index2users in machine2gpu_index2users.items() if isinstance(gpu_index2users, str)}
+    machine2gpu_index2users = {m: gpu_index2users for m, gpu_index2users in machine2gpu_index2users.items() if not m in machine2error_strs}
 
     machine2num_free_gpus = {m: sum([len(users) == 0 for users in gpu_index2users.values()]) for m, gpu_index2users in machine2gpu_index2users.items()}
     machine2gpu_index2users = sorted(machine2gpu_index2users.items(), key=lambda x: machine2num_free_gpus[x[0]], reverse=True)
@@ -104,6 +133,15 @@ def machine_gpu_usage_summary_str(*, machine2gpu_index2users=None):
         machine2usage_strs[machine] = free_gpu_str + " " * 20 + gpu_range_str
 
     machine2usage_str = "\t" + "\n\t\t\t".join([f"{machine}: {usage_str}" for machine, usage_str in machine2usage_strs.items()])
+
+    ##################################################################################
+    # Now add in the machines that hand an error
+    ##################################################################################
+    for machine, error_str in machine2error_strs.items():
+        machine_str = f"{machine}: GPU info unavailable: "
+        error_str = error_str.replace("\n", "\n\t\t\t" + " " * len(machine_str))
+        machine2usage_str += f"\n\t\t\t{machine_str}{error_str}"
+
     twrite(machine2usage_str)
     return machine2usage_str
 
@@ -118,6 +156,6 @@ if __name__ == "__main__":
 
     with Pool(processes=min(16, len(args.hosts))) as p:
         gpuindex2users = p.map(gpu_index_to_users, args.hosts, chunksize=math.ceil(len(args.hosts) / 16))
-        machine2gpu_index2users = {h: gpu_index2users for h, gpu_index2users in zip(args.hosts, gpuindex2users)}
+    machine2gpu_index2users = {h: gpu_index2users for h, gpu_index2users in zip(args.hosts, gpuindex2users)}
 
     _ = machine_gpu_usage_summary_str(machine2gpu_index2users=machine2gpu_index2users)
